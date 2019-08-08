662aec6a0429529eb32d175d7d0bcaa20b0ca9ee
NamidaEro <hongswin88@gmail.com>
2019-08-08 17:15:07 +0900

    test confirm

--- TestConsole/Main.cpp
+++ TestConsole/Main.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 
+#include "AString.h"
+
 using namespace std;
+using namespace RuEngine;
 
 int main()
 {
+	AString str("hello");
+
+	cout << str << endl;
+
+	system("pause");
 	return 0;
 }
